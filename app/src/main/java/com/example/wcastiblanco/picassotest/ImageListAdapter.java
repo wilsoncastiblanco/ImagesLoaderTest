f94f40abf06b77cefb927f94f0c7d0edc2d5fae1
Wilson Castiblanco Quintero <w.castiblanco@globant.com>
2016-10-14 16:36:17 -0500

    Picasso cache

--- app/src/main/java/com/example/wcastiblanco/picassotest/ImageListAdapter.java
+++ app/src/main/java/com/example/wcastiblanco/picassotest/ImageListAdapter.java
@@ -2,17 +2,20 @@ package com.example.wcastiblanco.picassotest;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
 /**
  * Created by w.castiblanco on 14/10/2016.
  */
-public class ImageListAdapter extends  RecyclerView.Adapter<ImageListAdapter.ViewHolder>  {
+public class ImageListAdapter extends  RecyclerView.Adapter<ImageListAdapter.ViewHolder> {
     private Context context;
     private String[] imageUrls;
 
@@ -24,7 +27,7 @@ public class ImageListAdapter extends  RecyclerView.Adapter<ImageListAdapter.Vie
     // Create new views (invoked by the layout manager)
     @Override
     public ImageListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
+                                                          int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.listview_item_image, parent, false);
@@ -35,13 +38,13 @@ public class ImageListAdapter extends  RecyclerView.Adapter<ImageListAdapter.Vie
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        Picasso.with(context)
-                .load(imageUrls[position])
-                .fit() // will explain later
-                .into(holder.imageView);
+
+        PicassoCache.getPicassoInstance(context).load(imageUrls[position]).into(holder.imageView);
+
+
 
     }
 
@@ -54,13 +57,14 @@ public class ImageListAdapter extends  RecyclerView.Adapter<ImageListAdapter.Vie
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public ImageView imageView;
+
         public ViewHolder(View v) {
             super(v);
             imageView = (ImageView) v.findViewById(R.id.imageView);
         }
     }
-
-
-
-
 }
+
+
+
+
